296eead8a8b9a7544af9befe61d912ab8f98a85e
Gustav Odinger <65498475+gustavwilliam@users.noreply.github.com>
2021-12-03 20:32:15 +0100

    Add solution for puzzle 2 and rewrite puzzle 1

--- 2021/day_3.py
+++ 2021/day_3.py
@@ -1,27 +1,50 @@
+from collections import Counter
+from itertools import compress
 from typing import List
-
 
 with open("2021/day_3.txt") as f:
     data = f.read().splitlines()
 
 
-def gamma(data: List[str]) -> int:
-    vals = [0] * len(data[0])
-    for row in data:
-        for i, val in enumerate(row):
-            vals[i] += 1 if val == "1" else -1
-
-    val = "".join(str(int(i >= 0)) for i in vals)
-    return int(val, base=2)
+def common(stream: List[str]) -> str:
+    count = Counter(stream).most_common()
+    if len(count) == 1 or count[0][1] != count[1][1]:
+        return count[0][0]
+    else:
+        return 1
 
 
-def epsilon(data: List[str]) -> int:
-    return ~gamma(data) & int("1" * len(data[0]), base=2)
+def puzzle_1(streams: List[List[str]]) -> int:
+    gamma = int("".join(common(s) for s in streams), base=2)
+    epsilon = ~gamma & int("1" * len(streams), base=2)
+    return gamma * epsilon
 
 
-def puzzle_1(data: List[str]) -> int:
-    return gamma(data) * epsilon(data)
+def puzzle_2(streams: List[List[str]], data: List[str]) -> int:
+    valid = [[True for _ in range(len(streams[0]))] for _ in range(2)]
+
+    for stream in streams:
+        o2 = common(list(compress(stream, valid[0])))
+        for j, row in enumerate(stream):
+            if valid[0].count(True) == 1:
+                break
+            if row != o2:
+                valid[0][j] = False
+
+    for stream in streams:
+        co2 = str(1 - int(common(list(compress(stream, valid[1])))))
+        for j, row in enumerate(stream):
+            if valid[1].count(True) == 1:
+                break
+            if row != co2:
+                valid[1][j] = False
+
+    return int(data[valid[0].index(True)], base=2) * int(
+        data[valid[1].index(True)], base=2
+    )
 
 
 if __name__ == "__main__":
-    print(f"Puzzle 1: {puzzle_1(data)}")
+    streams = [[row[i] for row in data] for i, _ in enumerate(data[0])]
+    print(f"Puzzle 1: {puzzle_1(streams)}")
+    print(f"Puzzle 2: {puzzle_2(streams, data)}")
