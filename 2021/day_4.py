325757421feb295b655518def62293a86c486048
Gustav Odinger <65498475+gustavwilliam@users.noreply.github.com>
2021-12-04 23:12:34 +0100

    Add solution 2 day 4

--- 2021/day_4.py
+++ 2021/day_4.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 from itertools import compress
-from typing import List, Tuple
+from typing import Generator, List, Tuple
 
 
 with open("2021/day_4.txt") as f:
@@ -14,6 +14,7 @@ class Board:
     def __post_init__(self):
         self.marked = [[False] * 5, [False] * 5, [False] * 5, [False] * 5, [False] * 5]
         self.bingo_value: int | None = None
+        self.completed = False
 
     def new_value(self, value: int) -> None:
         for i, row in enumerate(self.rows):
@@ -44,21 +45,28 @@ def parse_raw(data: List[str]) -> Tuple[List[int], List[Board]]:
     return inputs, [Board(dat) for dat in board_data]
 
 
-def get_bingo(data: Tuple[List[int], List[Board]]) -> Board:
+def get_all_bingo(data: Tuple[List[int], List[Board]]) -> Generator[Board, None, None]:
     for value in data[0]:
         for board in data[1]:
-            board.new_value(value)
-            if board.bingo():
-                board.bingo_value = value
-                return board
+            if not board.completed:
+                board.new_value(value)
+                if board.bingo():
+                    board.completed = True
+                    board.bingo_value = value
+                    yield board
 
 
 def puzzle_1(data: Tuple[List[int], List[Board]]) -> int:
-    board = get_bingo(data)
+    board = next(get_all_bingo(data))
+    return board.sum_unmarked * board.bingo_value
+
+
+def puzzle_2(data: Tuple[List[int], List[Board]]) -> int:
+    *_, board = get_all_bingo(data)
     return board.sum_unmarked * board.bingo_value
 
 
 if __name__ == "__main__":
     data = parse_raw(data)
     print(f"Puzzle 1: {puzzle_1(data)}")
-    # print(f"Puzzle 2: {puzzle_2(data)}")
+    print(f"Puzzle 2: {puzzle_2(data)}")
