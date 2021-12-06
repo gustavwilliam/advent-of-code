7fdea053de7940f8c506e9cd2c9a0130da5962e7
Gustav Odinger <65498475+gustavwilliam@users.noreply.github.com>
2021-12-06 20:53:39 +0100

    Rewrite day 6 and add puzzle 2 solution
    Tracking every single fish was... painful for the computer. So I started
    from scratch and made a nice and efficient solution. Some very sweet
    logic went into this, and I thank my partner in crime—The Whiteboard—for
    helping me out with the thinking phase. Without the incredible support
    from Mr. Whiteboard, I most likely wouldn't have completed this
    difficult task.

--- 2021/day_6.py
+++ 2021/day_6.py
@@ -1,25 +1,31 @@
-from itertools import chain
+from collections import Counter
 from typing import List
-
 
 with open("2021/day_6.txt") as f:
     data = [int(fish) for fish in f.read().split(",")]
 
 
-def increment_age(timer: int) -> List[int]:
-    return [6, 8] if timer == 0 else [timer - 1]
+def fish_count(data: List[int], days: int) -> int:
+    fish = [0] * 9
+    for i, val in Counter(data).items():
+        fish[i] = val
 
-
-def puzzle_1(data: List[int], days: int) -> int:
     for _ in range(days):
-        data = list(chain(*[increment_age(fish) for fish in data]))
-    return len(data)
+        new = fish.pop(0)
+        fish[6] += new
+        fish.append(new)
+
+    return sum(fish)
+
+
+def puzzle_1(data: List[int]) -> int:
+    return fish_count(data, 80)
 
 
 def puzzle_2(data: List[int]) -> int:
-    ...
+    return fish_count(data, 256)
 
 
 if __name__ == "__main__":
-    print(f"Puzzle 1: {puzzle_1(data, 80)}")
-    # print(f"Puzzle 2: {puzzle_2(data)}")
+    print(f"Puzzle 1: {puzzle_1(data)}")
+    print(f"Puzzle 2: {puzzle_2(data)}")
