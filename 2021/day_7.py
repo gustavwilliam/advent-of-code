b54dbca5b2e528d4afa8c330a043bc4adc078924
Gustav Odinger <65498475+gustavwilliam@users.noreply.github.com>
2021-12-07 15:27:42 +0100

    Add day 7 puzzle 1 solution

---
+++ 2021/day_7.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+with open("2021/day_7.txt") as f:
+    data: np.ndarray = np.fromiter(f.read().split(","), dtype=int)
+
+
+def mean(data: np.ndarray) -> int:
+    median = int(np.median(data))
+    lowest: int | None = None
+    for guess in range(median - 10, median + 10):
+        cost = sum(map(abs, data - np.full(len(data), guess)))
+        if lowest is None:
+            lowest = cost
+        elif cost < lowest:
+            lowest = cost
+
+    return lowest or 0
+
+
+def puzzle_1(data: np.ndarray) -> int:
+    return mean(data)
+
+
+def puzzle_2(data: np.ndarray) -> int:
+    ...
+
+
+if __name__ == "__main__":
+    print(f"Puzzle 1: {puzzle_1(data)}")
+    # print(f"Puzzle 2: {puzzle_2(data)}")
